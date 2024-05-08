b1334dae6948888d873fb477bc6100f1b335ecd9
Stefan Anca <bilbo7833@gmail.com>
2024-05-08 21:13:09 +0200

    Adapted all helper classes and working on main loop WIP

--- java/src/main/java/com/datasqrl/ai/models/bedrock/Llama3MessageFormatter.java
+++ java/src/main/java/com/datasqrl/ai/models/bedrock/Llama3MessageFormatter.java
@@ -4,22 +4,22 @@ import com.datasqrl.ai.models.ChatMessageFormatter;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class Llama3MessageFormatter implements ChatMessageFormatter {
+public class Llama3MessageFormatter implements ChatMessageFormatter<BedrockChatMessage> {
 
-  public String formatMessage(BedrockChatRole role, String content) {
-    return switch (role) {
+  public String formatMessage(BedrockChatMessage message) {
+    return switch (message.getRole()) {
       case USER -> "<|start_header_id|>user<|end_header_id|>\n"
-          + content + "\n"
+          + message.getTextContent() + "\n"
           + "<|eot_id|>\n";
       case ASSISTANT -> "<|start_header_id|>assistant<|end_header_id|>\n"
-          + content + "\n"
+          + message.getTextContent() + "\n"
           + "<|eot_id|>\n";
       case FUNCTION ->"<|start_header_id|>function<|end_header_id|>\n"
-          + content + "\n"
+          + message.getTextContent() + "\n"
           + "<|eot_id|>\n";
       case SYSTEM -> "<|begin_of_text|>\n"
           + "<|start_header_id|>system<|end_header_id|>\n"
-          + content + "\n"
+          + message.getTextContent() + "\n"
           + "<|eot_id|>\n";
     };
   }
