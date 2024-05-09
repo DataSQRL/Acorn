8076767f8022f02ec54c9ab301b93642da9a4b90
Stefan Anca <bilbo7833@gmail.com>
2024-05-09 13:28:51 +0200

    Final cleanup to make the round-trip communication work

--- java/src/main/java/com/datasqrl/ai/models/ChatMessageFormatter.java
+++
@@ -1,6 +0,0 @@
-package com.datasqrl.ai.models;
-
-public interface ChatMessageFormatter<Message> {
-  String encodeMessage(Message message);
-  Message decodeMessage(String message, String roleHint);
-}
