5109f73f3916142614569e3cd56651140c954e7e
Stefan Anca <bilbo7833@gmail.com>
2024-05-13 16:36:52 +0200

    Increased the context length to 1024 to accommodate a longer LLM answer

--- java/src/main/java/com/datasqrl/ai/models/groq/GroqChatModel.java
+++ java/src/main/java/com/datasqrl/ai/models/groq/GroqChatModel.java
@@ -15,7 +15,7 @@ public enum GroqChatModel implements GenericLanguageModel {
 //  LLAMA3_70B("llama3-70b-8192", "meta-llama/Meta-Llama-3-70B-Instruct", 8192, 512),
 //  MIXTRAL_8x7B("mixtral-8x7b-32768", "mistralai/Mixtral-8x7B-Instruct-v0.1", 32768, 512),
 //  GEMMA_7B("gemma-7b-it", "google/gemma-1.1-7b-it", 8192, 512),
-  LLAMA3_70B("llama3-70b-8192", "meta-llama/Meta-Llama-3-8B-Instruct", 8192, 512),
+  LLAMA3_70B("llama3-70b-8192", "meta-llama/Meta-Llama-3-8B-Instruct", 8192, 1024),
   MIXTRAL_8x7B("mixtral-8x7b-32768", "meta-llama/Meta-Llama-3-8B-Instruct", 32768, 512),
   GEMMA_7B("gemma-7b-it", "meta-llama/Meta-Llama-3-8B-Instruct", 8192, 512),
   LLAMA3_7B("llama3-8b-8192", "meta-llama/Meta-Llama-3-8B-Instruct", 8192, 512);
