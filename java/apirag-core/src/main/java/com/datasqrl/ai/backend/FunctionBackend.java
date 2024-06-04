f47ac6131b29fb3bb5b1fdec96a889617dcc5547
Stefan Anca <bilbo7833@gmail.com>
2024-06-04 13:52:31 +0200

    Merge cleanup

--- java/apirag-core/src/main/java/com/datasqrl/ai/backend/FunctionBackend.java
+++ java/apirag-core/src/main/java/com/datasqrl/ai/backend/FunctionBackend.java
@@ -70,7 +70,8 @@ public class FunctionBackend {
   public static FunctionBackend of(@NonNull String tools, @NonNull APIExecutor apiExecutor) throws IOException {
     ObjectMapper mapper = new ObjectMapper();
     List<RuntimeFunctionDefinition> functions = mapper.readValue(tools,
-        new TypeReference<List<RuntimeFunctionDefinition>>(){});
+        new TypeReference<>() {
+        });
     return new FunctionBackend(functions.stream()
         .filter(f -> !RESERVED_FUNCTION_NAMES.contains(f.getName().toLowerCase()))
         .collect(Collectors.toMap(RuntimeFunctionDefinition::getName, Function.identity())),
