5163bed065f6a518fdcd92ed7e1e73bf5756802a
Stefan Anca <bilbo7833@gmail.com>
2024-06-12 15:04:22 +0200

    Added all classes needed for Google provider

---
+++ java/apirag-google/src/main/java/com/datasqrl/ai/models/google/GoogleChatProviderFactory.java
@@ -0,0 +1,31 @@
+package com.datasqrl.ai.models.google;
+
+import com.datasqrl.ai.backend.FunctionBackend;
+import com.datasqrl.ai.models.AbstractChatProviderFactory;
+import com.datasqrl.ai.models.ChatClientProvider;
+import com.datasqrl.ai.models.ChatProviderFactory;
+import com.datasqrl.ai.util.ErrorHandling;
+import com.google.auto.service.AutoService;
+import org.apache.commons.configuration2.Configuration;
+
+@AutoService(ChatProviderFactory.class)
+public class GoogleChatProviderFactory extends AbstractChatProviderFactory {
+
+  public static final String PROVIDER_NAME = "google";
+  private static final String VERTEX_PROJECT_ID_KEY = "vertex-project-id";
+  private static final String VERTEX_LOCATION_KEY = "vertex-location";
+
+  @Override
+  public String getProviderName() {
+    return PROVIDER_NAME;
+  }
+
+  @Override
+  public ChatClientProvider<?, ?> create(Configuration modelConfiguration, FunctionBackend backend, String prompt) {
+    String vertexProjectId = modelConfiguration.getString(VERTEX_PROJECT_ID_KEY);
+    ErrorHandling.checkArgument(vertexProjectId != null && !vertexProjectId.isBlank(), "Need to configure vertex-project-id.");
+    String vertexLocation = modelConfiguration.getString(VERTEX_LOCATION_KEY);
+    ErrorHandling.checkArgument(vertexLocation != null && !vertexLocation.isBlank(), "Need to configure vertex-location.");
+    return new GoogleChatProvider(getModel(modelConfiguration, GoogleChatModel.class), vertexProjectId, vertexLocation, backend, prompt);
+  }
+}
