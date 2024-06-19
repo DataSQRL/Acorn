529f93b36d348924f74f6db668490240083bc855
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2024-06-18 17:56:09 -0700

    Fixed json de/serialization for GenericFunctionCall. Added test case

--- java/apirag-core/src/main/java/com/datasqrl/ai/backend/GenericFunctionCall.java
+++ java/apirag-core/src/main/java/com/datasqrl/ai/backend/GenericFunctionCall.java
@@ -1,6 +1,8 @@
 package com.datasqrl.ai.backend;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
@@ -18,13 +20,20 @@ public class GenericFunctionCall {
   String name;
   JsonNode arguments;
 
+  @JsonCreator
+  public static GenericFunctionCall create(String jsonString) throws JsonProcessingException {
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode jsonNode = mapper.readTree(jsonString);
+    String name = jsonNode.get("name").asText();
+    JsonNode arguments = jsonNode.get("arguments");
+    return new GenericFunctionCall(name, arguments);
+  }
+
 
   @SneakyThrows
-  @JsonValue
+  @Override
   public String toString() {
-    return "{"
-        + "\"name\": \"" + name + "\", "
-        + "\"arguments\": " + new ObjectMapper().writeValueAsString(arguments)
-        + "}";
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.writeValueAsString(this);
   }
 }
