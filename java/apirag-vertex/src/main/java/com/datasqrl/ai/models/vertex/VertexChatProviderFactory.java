782029c8cda7950a647a33dd66af63a92921d68e
Stefan Anca <bilbo7833@gmail.com>
2024-07-12 23:18:56 +0200

    Updated the configuration names

--- java/apirag-vertex/src/main/java/com/datasqrl/ai/models/vertex/VertexChatProviderFactory.java
+++ java/apirag-vertex/src/main/java/com/datasqrl/ai/models/vertex/VertexChatProviderFactory.java
@@ -1,15 +1,13 @@
 package com.datasqrl.ai.models.vertex;
 
 import com.datasqrl.ai.backend.FunctionBackend;
-import com.datasqrl.ai.models.AbstractChatProviderFactory;
 import com.datasqrl.ai.models.ChatClientProvider;
 import com.datasqrl.ai.models.ChatProviderFactory;
-import com.datasqrl.ai.util.ErrorHandling;
 import com.google.auto.service.AutoService;
 import org.apache.commons.configuration2.Configuration;
 
 @AutoService(ChatProviderFactory.class)
-public class VertexChatProviderFactory extends AbstractChatProviderFactory {
+public class VertexChatProviderFactory implements ChatProviderFactory {
 
   public static final String PROVIDER_NAME = "vertex";
 
