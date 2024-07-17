96af138e3dc8d615a65bae7401e18eb68887e609
Stefan Anca <bilbo7833@gmail.com>
2024-07-17 23:04:23 +0200

    Made the RequestBody null for GET methods

--- java/apirag-starter/src/main/java/com/datasqrl/ai/api/RESTExecutor.java
+++ java/apirag-starter/src/main/java/com/datasqrl/ai/api/RESTExecutor.java
@@ -4,13 +4,13 @@ import com.datasqrl.ai.api.RestUtil.RestCall;
 import com.datasqrl.ai.util.ErrorHandling;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Strings;
-import java.io.IOException;
-import java.util.Optional;
 import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+
+import java.io.IOException;
 
 @SuperBuilder
 public class RESTExecutor extends AbstractOkhttpExecutor {
@@ -41,7 +41,7 @@ public class RESTExecutor extends AbstractOkhttpExecutor {
 
   private RequestBody buildRequestBody(JsonNode requestBody, String method) throws IOException {
     if (requestBody == null || requestBody.isEmpty() || method.equalsIgnoreCase("GET")) {
-      return RequestBody.create("", null);
+      return null;
     }
     return RequestBody.create(objectMapper.writeValueAsString(requestBody), MediaType.get("application/json; charset=utf-8"));
   }
